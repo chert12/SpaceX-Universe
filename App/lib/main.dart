2cbc28bf6514bb486bdabf16a4f5cf00ca88cf43
chert12 <aastudio15@gmail.com>
2019-03-25 23:45:53 +0200

    add flutter project

---
+++ App/lib/main.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:spacex_universe/services/AppConstants.dart';
+
+void main() => runApp(SpaceXUniverseApp());
+
+class SpaceXUniverseApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'SpaceX Universe',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'SpaceX Universe'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      drawer: Drawer(
+          child:new DrawerHeader(
+            child: ListView(
+              children: <Widget>[
+                ListTile(
+                  title: Text("Last launch", style: TextStyle(
+                      color: Colors.white
+                  ),),
+                  trailing: Icon(Icons.arrow_forward),
+                ),
+                ListTile(
+                  title: Text("Launches", style: TextStyle(
+                      color: Colors.white
+                  ),),
+                  trailing: Icon(Icons.arrow_forward),
+                ),
+                ListTile(
+                  title: Text("Rockets", style: TextStyle(
+                      color: Colors.white
+                  ),),
+                  trailing: Icon(Icons.arrow_forward),
+                ),
+                ListTile(
+                  title: Text("Capsules", style: TextStyle(
+                      color: Colors.white
+                  ),),
+                  trailing: Icon(Icons.arrow_forward),
+                ),
+                ListTile(
+                  title: Text("Dragons", style: TextStyle(
+                      color: Colors.white
+                  ),),
+                  trailing: Icon(Icons.arrow_forward),
+                ),
+                ListTile(
+                  title: Text("Launch pads", style: TextStyle(
+                      color: Colors.white
+                  ),),
+                  trailing: Icon(Icons.arrow_forward),
+                ),
+                ListTile(
+                  title: Text("Landing pads", style: TextStyle(
+                      color: Colors.white
+                  ),),
+                  trailing: Icon(Icons.arrow_forward),
+                ),
+                ListTile(
+                  title: Text("About SpaceX", style: TextStyle(
+                      color: Colors.white
+                  ),),
+                  trailing: Icon(Icons.arrow_forward),
+                ),
+              ],
+            ),
+            decoration: new BoxDecoration(
+                color: Colors.blue
+            ),
+          )
+      ),
+    );
+  }
+}
---
+++ App/lib/services/AppConstants.dart
@@ -0,0 +1,6 @@
+class AppConstants
+{
+  static const String API_GET_ALL_CAPSULES = "https://api.spacexdata.com/v3/capsules";
+  static const String API_GET_UPCOMING_CAPSULES = "https://api.spacexdata.com/v3/capsules/upcoming";
+  static const String API_GET_PAST_CAPSULES = "https://api.spacexdata.com/v3/capsules/past";
+}
